05a5d13d898fe682b7cff3393d62107c4a2817df
lukas <l.ewald@bringe.com>
2018-03-05 10:25:08 +0100

    Update Version 0.0001 nach großer Änderung

--- js/script.js
+++ js/script.js
@@ -1,96 +1,155 @@
-/**
-	Login von Lukas
+/******
+ *
+ * Part von Lukas
+ *
+ * Datum: 2018-02-26
+ *
 */
+
+/**
+ * TODO:
+ *  *Bennenung überdenken
+ *  *UserView,ListsView und View für einzelne Elemenete vorbereiten, diese dann per JSON Füllen
+ *  *Kommentierung
+ *
+ *  *Update Elemente
+ *  *Update Liste
+ *  *Update User
+ */
+
+
+// region Globales und document Ready
+let USERTOKEN = null;
+let USERNAME = null;
+
+//Document Ready
 $( document ).ready(function() {
 	checkLogin();
 });
 
-function loadUserView(){
+// Set up some default data for jQuery-driven AJAX requests.
+// In general, ajaxSetup() builds the options hash; but when
+// it comes to data, the effect is accumulative.
+$.ajaxSetup({
+	data: {
+		token: USERTOKEN,
+	},
+	dataType: "jsonp"
+});
+
+//endregion
+
+//region login und Startseite
+function checkLogin(){
+	let username = "lukas";
+	let password = "123";
+
+	$.ajax({
+		data: {username:username,password:password},
+		//url: "user/login", //TODO: Change URI
+		url: "demo/login.json", //TODO: Change URI
+		method:"get",
+		dataType: "json"
+	}).fail(function() {
+		alert( "error" );
+		viewLogin(false);
+	}).done(function(dat) {
+		USERTOKEN = dat.token;
+		USERNAME = dat.username;
+
+		if(USERTOKEN !== null){
+			viewMain();
+		}
+		else{
+			viewLogin(false);
+		}
+	});
+
+
+}
+
+function viewLogin(){
+	$.ajax({
+		url: "login.html",
+		dataType: "html"
+	}).fail(function() {
+		alert( "error" );
+	}).done(function(data) {
+		document.querySelector("#myMainContent").innerHTML = data; //lädt Daten in HTML
+	});
+}
+
+function viewMain() {
+	$.ajax({
+		url: "mainView.html", //TODO: Change URI
+		dataType: "html"
+	}).fail(function() {
+		alert( "error" );
+	}).done(function(data) {
+		document.querySelector("#myMainContent").innerHTML = data;
+		viewUser();
+		viewAllLists();
+	}).always(function() {
+		document.querySelector("#myUsernameP").innerHTML= "Test";
+	});
+}
+//endregion
+
+
+//region Liste
+function viewAllLists(){
+	$.ajax({
+		url: "listViewAll.html",
+		dataType: "html"
+	}).fail(function() {
+		alert( "error" );
+	}).done(function(data) {
+		document.querySelector("#listen-div").innerHTML = data;
+		getLitsts();
+	});
+}
+
+function getLitsts(){
+	$.ajax({
+		url: "demo/lists.json", //TODO: Change URI
+	}).fail(function() {
+		alert( "error" );
+	}).done(function(data) {
+		let ul = document.querySelector("#listView-list-ul");
+		ul.innerHTML = "";
+		for(let i = 0; i<data.length;i++){
+			ul.innerHTML += `<a href="javascript:void(0)" onclick="showListDetails(${data[i].id})">${data[i].name}</a><br>`
+		}
+	});
+}
+
+function addNewList(title){
+	alert(title);
+
+	console.log("Hier kommt dann ein POST");
+}
+//endregion
+
+//region Elements
+function getElementsFromList(listID){
+	console.log("Hier stehen dann die Elemente von einer Liste");
+}
+
+function viewList(listID){
+	getElementsFromList(listID);
+}
+//endregion
+
+//region User
+function viewUser(){
 	$.ajax({
 		url: "userView.html",
 		dataType: "text"
-	}).fail(function() {
+	}).fail(function(err) {
 		alert( "error" );
+		console.log(err);
 	}).done(function(data) {
 		document.querySelector("#user-div").innerHTML = data;
 	});
 }
-
-function loadListView(){
-	$.ajax({
-		url: "listView.html",
-		dataType: "text"
-	}).fail(function() {
-		alert( "error" );
-	}).done(function(data) {
-		document.querySelector("#user-div").innerHTML = data;
-	});
-}
-
-function checkLogin(){
-	var login = true;
-	var mainContent = document.querySelector("#myMainContent");
-	var html = "Bitte warten...";
-	mainContent.innerHTML = html;
-
-	if(login){
-		$.ajax({
-			url: "mainView.html",
-			dataType: "html"
-		}).fail(function() {
-			alert( "error" );
-		}).done(function(data) {
-			mainContent.innerHTML = data;
-			loadUserView();
-			loadListView();
-		});
-	}
-	else{
-		$.ajax({
-			url: "login.html",
-			dataType: "html"
-		}).fail(function() {
-			alert( "error" );
-		}).done(function(data) {
-			mainContent.innerHTML = data;
-		});
-	}
-
-}
-
-$( "#form-login" ).submit(function( event ) {
-	var elements = $( this ).serializeArray();
-	//elements[0] => usernameInput
-	//elements[1] => password
-	if(elements[1].value === "password" && elements[0].value === "user"){
-		return true; //lädt neue Seite
-	}
-	else{
-		event.preventDefault();
-		return false;
-	}
-});
-
-/**
-	Register von Lukas
-*/
-$( "#form-register" ).submit(function( event ) {
-	var elements = $( this ).serializeArray();
-	console.log(elements);
-	alert("Registriere User ...");
-  event.preventDefault();
-});
-
-/**
-	Lade Listen
-*/
-function getLists(userToken){
-
-}
-
-/**
-	Lade Elemente von Liste
-*/
-function getElementsFromList(userToken,listID){
-	
-}
+//endregion

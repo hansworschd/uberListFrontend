b779fd80a1ffffaef76677c5af47aa7a86484f98
lukas <l.ewald@bringe.com>
2018-03-27 14:37:30 +0200

    Großes Update

--- js/script.js
+++ js/script.js
@@ -22,7 +22,7 @@
  */
 
 
-// region Globales und document Ready
+// region Globales
 /**
  * Globale Variablen
  */
@@ -31,45 +31,36 @@ let USERNAME = null;
 
 let URL = "http://uberlistwebapi.azurewebsites.net/";
 
-
 function myFetch(backendMethod, data, type) {
 	//waitingDialog.show();
 	let url = URL + backendMethod;
 
-	if(data !== null && data !== undefined){
+	if (data !== null && data !== undefined) {
+		console.log("Stringify");
 		data = JSON.stringify(data);
 	}
-	else{
+	else {
 		data = undefined;
 	}
-  // Default options are marked with *
-  return fetch(url, {
-    body: data, // must match 'Content-Type' header
-    headers: {
-      'content-type': 'application/json',
+	// Default options are marked with *
+
+	//waitingDialog.hide();
+	return fetch(url, {
+		body: data, // must match 'Content-Type' header
+		headers: {
+			'content-type': 'application/json',
 			'authorization': USERTOKEN,
-    },
-		mode: 'cors',
-    method: type, // *GET, POST, PUT, DELETE, etc.
-  }).then(response => response.json()).catch(error => console.log(error)); // parses response to JSON
+		},
+		method: type, // *GET, POST, PUT, DELETE, etc.
+	}).then(response => response.json()).catch(error => console.log(error)); // parses response to JSON
 }
 
-
-/**
- * Document Ready - Prüfe Login
- */
-$(document).ready(function () {
-	document.querySelector("#mainContent").style.display = 'none';
-	login();
-
-});
-//endregion
 
 //region login und Startseite
 /**
  * Check den Login --> True: mainView, false: Login
  */
-function login(){
+function login() {
 	let username = document.querySelector("#loginUsername").value;
 	let password = document.querySelector("#loginPassword").value;
 
@@ -90,27 +81,29 @@ function login(){
 			username: username,
 			password: password,
 		};
-		myFetch('authenticate', data, "POST").then(function(dat){
+		myFetch('authenticate', data, "POST").then(function (dat) {
 			console.log(dat);
-				USERTOKEN = dat.token;
-				USERNAME = dat.username;
-				//Ändern der Darstellung zwischen Main (Normale Seiten) und Login (ausgeloggt)
-				document.querySelector("#mainContent").style.display = 'block';
-				document.querySelector("#loginContent").style.display = 'none';
-				//getUserData();
-				getLitsts();
-				setBackgroundColor(dat.color);
-			}).catch(function(error) { console.log(error); });
+			USERTOKEN = dat.token;
+			USERNAME = dat.username;
+			//Ändern der Darstellung zwischen Main (Normale Seiten) und Login (ausgeloggt)
+			document.querySelector("#mainContent").style.display = 'block';
+			document.querySelector("#loginContent").style.display = 'none';
+			getUserData();
+			getLitsts();
+			setBackgroundColor(dat.color);
+		}).catch(function (error) {
+			console.log(error);
+		});
 	}
 }
 
-$("#form-login").submit(function(e) {
+$("#form-login").submit(function (e) {
 	e.preventDefault();
 	login();
 });
 
 
-$("#form-register").submit(function(e) {
+$("#form-register").submit(function (e) {
 	e.preventDefault();
 
 	let username = document.querySelector("#registerUsername").value;
@@ -128,21 +121,19 @@ $("#form-register").submit(function(e) {
 			email: email,
 		};
 		myFetch('register', sendData, "POST")
-			.then(function(dat){
+			.then(function (dat) {
 				alert("Registriert");
 			}) // JSON from `response.json()` call
-
 	}
 });
 
-function logout(){
+function logout() {
 	myFetch('authenticate', null, "DELETE")
-		.then(function(){
+		.then(function () {
 			document.querySelector("#mainContent").style.display = 'none';
 			document.querySelector("#loginContent").style.display = 'block';
 		});
 }
-
 //endregion
 
 
@@ -156,16 +147,25 @@ function getLitsts() {
 		.then(function (data) {
 			console.log(data)
 			let lists = data.lists;
-				let ul = document.querySelector("#listView-list-ul");
-				ul.innerHTML = "";
-				for (let i = 0; i < lists.length; i++) {
-					ul.innerHTML += `<li> <a href="javascript:void(0)" onclick="showListDetails()"><i class="fas fa-pencil-alt"></i></a> <a href="javascript:void(0)" onclick="showElements(${lists[i]._id})">${lists[i].title}</a></li>`
-				}
+			let ul = document.querySelector("#listView-list-ul");
+			ul.innerHTML = "";
+			for (let i = 0; i < lists.length; i++) {
+				ul.innerHTML += `<li> <a href="javascript:void(0)" onclick="showListDetails('${lists[i]._id}')"><i class="fas fa-pencil-alt"></i></a> <a href="javascript:void(0)" onclick="showElements('${lists[i]._id}')">${lists[i].title}</a></li>`
+			}
 		});
 }
 
-function showListDetails() {
+function showListDetails(id) {
+	myFetch('secure/list/'+id, null, "GET")
+		.then(function (data) {
+			let list = data.list;
+			document.querySelector("#listEditID").value = list._id;
+			document.querySelector("#listEditName").value = list.title;
+		});
+
 	$('#listDetailsModal').modal("toggle");
+
+
 }
 
 $('.nav-tabs a').on('shown.bs.tab', function () {
@@ -176,6 +176,40 @@ $('.nav-tabs a').on('shown.bs.tab', function () {
 function showLists() {
 	document.querySelector("#showElements").style.display = "none";
 	document.querySelector("#showListen").style.display = "block";
+}
+
+function updateList(){
+	let id = document.querySelector("#listEditID").value;
+	let data = {
+		title: document.querySelector("#listEditName").value,
+		text: document.querySelector("#listEditFreetext").value,
+	};
+
+	myFetch('secure/list/'+id, data, "PATCH")
+		.then(function (data) {
+			new PNotify({
+				title: 'Success!',
+				text: 'Die Liste wurde aktualisiert.',
+				type: 'success'
+			});
+		})
+		.then($('#listDetailsModal').modal("toggle"))
+		.then(getLitsts());
+}
+
+function deleteList() {
+	let id = document.querySelector("#listEditID").value;
+	myFetch('secure/list/'+id, null, "DELETE")
+		.then($('#listDetailsModal').modal("toggle"))
+		.then(getLitsts())
+		.then(
+			new PNotify({
+				title: 'Delete!',
+				text: 'Die Liste wurde gelöscht.',
+				type: 'warning'
+			})
+		);
+
 }
 
 /**
@@ -189,18 +223,20 @@ function addNewList(element) {
 		//alert("Die Liste " + title + " wird angelegt.");
 
 		myFetch('secure/list', {title: title}, "POST")
-			.then(data => function (data) {
-				console.log(data);
+			.then(function (data) {
+				new PNotify({
+					title: 'Success!',
+					text: 'Eine neue Liste wurde angelegt.',
+					type: 'success'
+				});
 				getLitsts();
 				element.value = "";
 			}) // JSON from `response.json()` call
 			.catch(error => console.error(error))
-		}
+	}
 }
 
-function deleteList() {
 
-}
 
 //endregion
 
@@ -214,9 +250,8 @@ function showElements(listID) {
 }
 
 function getElementsFromList(listID) {
-
 	myFetch('secure/getElements', {listId: listID}, "POST")
-		.then(data => function (data) {
+		.then(function (data) {
 			console.log(data);
 
 			document.querySelector("#myCurrentList").value = listID;
@@ -232,7 +267,7 @@ function getElementsFromList(listID) {
 			ul.innerHTML = "";
 			for (let i = 0; i < data['elements'].length; i++) {
 				let dat = data['elements'][i];
-				ul.innerHTML += `<li><a href="javascript:void(0)" onclick="showElementDetails(${dat.id})"><i class="fas fa-pencil-alt"></i></a> <a href="javascript:void(0)" onclick="checkElement(${dat.id})"><i class="fas fa-check"></i></a> ${dat.name}</li>`
+				ul.innerHTML += `<li><a href="javascript:void(0)" onclick="showElementDetails('${dat._id}')"><i class="fas fa-pencil-alt"></i></a> <a href="javascript:void(0)" onclick="checkElement('${dat._id}')"><i class="fas fa-check"></i></a> ${dat.name}</li>`
 			}
 		}) // JSON from `response.json()` call
 		.catch(error => console.error(error));
@@ -260,10 +295,8 @@ function checkElement(id) {
 	//TODO ajax Check#
 
 	myFetch('secure/checkElement', {element: id}, "POST")
-		.then(data => function (data) {
-			console.log(data);
-			getLitsts();
-			element.value = "";
+		.then(function (data) {
+
 		}) // JSON from `response.json()` call
 		.catch(error => console.error(error));
 }
@@ -272,10 +305,12 @@ function deleteElement(id) {
 	//TODO ajax delete
 
 	myFetch('secure/deleteElement', {element: id}, "POST")
-		.then(data => function (data) {
-			console.log(data);
-			getLitsts();
-			element.value = "";
+		.then(function (data) {
+			new PNotify({
+				title: 'Delete!',
+				text: 'Das Element wurde gelöscht',
+				type: 'warning'
+			});
 		}) // JSON from `response.json()` call
 		.catch(error => console.error(error));
 }
@@ -284,8 +319,12 @@ function updateElement() {
 	//TODO fetch update
 
 	myFetch('secure/updateElement', data, "POST")
-		.then(data => function (data) {
-			console.log(data);
+		.then(function (data) {
+			new PNotify({
+				title: 'Success!',
+				text: 'Das Element wurde aktualisiert.',
+				type: 'success'
+			});
 			getLitsts();
 			element.value = "";
 		}) // JSON from `response.json()` call
@@ -299,8 +338,12 @@ function addNewElement(element) {
 		alert("Hier wird dann das Element " + text + " an die Liste " + listID + " angefügt");
 
 		myFetch('secure/addElement', {list: listID, element: text}, "POST")
-			.then(data => function (data) {
-				console.log(data);
+			.then(function (data) {
+				new PNotify({
+					title: 'Success!',
+					text: 'Ein neues Element wurde angefügt.',
+					type: 'success'
+				});
 				getLitsts();
 				element.value = "";
 			}) // JSON from `response.json()` call
@@ -315,21 +358,50 @@ function addNewElement(element) {
  * Lade Userdaten
  */
 function getUserData() {
-	/*myFetch('secure/getUser', {}, "GET")
-		.then(data => function (data) {
-			document.querySelector("#user_username").value = data.username;
-			document.querySelector("#user_password").value = data.password;
-			document.querySelector("#user_name").value = data.name;
-			document.querySelector("#user_anschrift").value = data.strasse;
-			document.querySelector("#user_plz").value = data.plz;
-			document.querySelector("#user_ort").value = data.ort;
-			document.querySelector("#user_telefon").value = data.telefonnummer;
-			document.querySelector("#user_email").value = data.email;
-			document.querySelector("#user_geburtsdatum").value = data.geburtsdatum;
+	myFetch('secure/user', null, "GET")
+		.then(function (data) {
+			let user = data.user;
+			document.querySelector("#user_username").value = user.username;
+			document.querySelector("#user_name").value = user.name;
+			document.querySelector("#user_anschrift").value = user.strasse;
+			document.querySelector("#user_plz").value = user.plz;
+			document.querySelector("#user_ort").value = user.ort;
+			document.querySelector("#user_telefon").value = user.telefonnummer;
+			document.querySelector("#user_email").value = user.email;
+			document.querySelector("#user_geburtsdatum").value = user.gebDate;
 		}) // JSON from `response.json()` call
 		.catch(error => console.error(error));
-		*/
 }
+
+$("#form-user-data").submit(function(e){
+	e.preventDefault();
+	let update = {
+		"name": document.querySelector("#user_name").value,
+		"strasse": document.querySelector("#user_anschrift").value,
+		"plz": document.querySelector("#user_plz").value,
+		"ort": document.querySelector("#user_ort").value,
+		"email": document.querySelector("#user_email").value,
+		"gebDate": document.querySelector("#user_geburtsdatum").value,
+	};
+
+	let password = document.querySelector("#user_password").value;
+	if(password !== ""){
+		update.password = password;
+	}
+
+
+	myFetch('secure/user', update , "POST")
+		.then(function (data) {
+			new PNotify({
+				title: 'Success!',
+				text: 'Die geänderten Daten wurden gespeichert',
+				type: 'success'
+			});
+			getUserData();
+		})
+		.catch(error => console.error(error));
+});
+
 
 /**
  * setBackgroundColor --> Hintergrundfarbe setzen
@@ -349,9 +421,9 @@ function setBackgroundColor(color) {
  * @param element --> DOM Element auf das die Suche angewendet werden soll
  */
 function searchForUserName(search, element) {
-	myFetch('secure/searcUser', {"search":search}, "GET")
-		.then(data => function (data) {
-			$(element).typeahead({source: data});
+	myFetch('secure/user/search/'+search, null, "GET")
+		.then(function (data) {
+			$(element).typeahead({source: data.data});
 		}) // JSON from `response.json()` call
 		.catch(error => console.error(error));
 }
@@ -360,6 +432,9 @@ function searchForUserName(search, element) {
 
 //region Onload
 $(function () {
+	document.querySelector("#mainContent").style.display = 'none';
+
+
 	$('#elementEditTimePicker').datetimepicker({
 		locale: 'de',
 		icons: {
@@ -381,8 +456,12 @@ $(function () {
 		format: "DD.MM.YYYY",
 	});
 
+	$('#user_backgroundcolor').colorpicker({
+		format: "hex",
+	});
 
+	PNotify.prototype.options.delay = 2000;
+
+	login();
 });
-
-
 //endregion

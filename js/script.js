f9ba683c9a85f53290b9ec3088bf464d84c6d06b
Hans Worschd <hansWorschd@noMail.de>
2018-04-05 17:09:37 +0200

    Kommentare und kleiner Änderungen

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@
  *
  * Part von Lukas E. und Vera G.
  *
- * Datum: 2018-03-28
+ * Datum: 2018-04-05
  *
  */
 // region Globales und onLoad
@@ -27,12 +27,13 @@ function myFetch(backendMethod, data, type) {
 	let url = URL + backendMethod;
 
 	if (data !== null && data !== undefined) {
-		data = JSON.stringify(data);
+		data = JSON.stringify(data); //Server akzeptiert nur JSON als String
 	}
 	else {
 		data = undefined;
 	}
 
+//Eigentlicher ajx Call
 	let res = $.ajax({
 		url: url,
 		type: type,
@@ -40,6 +41,7 @@ function myFetch(backendMethod, data, type) {
 		cache: false,
 		timeout: 30000,
 		data: data,
+		//wird für die Authentifizierung benötigt
 		beforeSend: function(request) {
 			request.setRequestHeader("authorization", USERTOKEN);
 			request.setRequestHeader("content-type", 'application/json');
@@ -64,8 +66,10 @@ function myFetch(backendMethod, data, type) {
  * Initialisieren von Werten und Deaktivieren von MainContent
  */
 $(function () {
+	//MainContent ausblenden --> sonst sehen nicht eingeloggte User diverse Dinge; Auth jedoch auf Server und Client seite, daher recht sicher
 	document.querySelector("#mainContent").style.display = 'none';
 
+	//EInstellungen für die Datetimepicker
 	$('#elementEditTimePicker').datetimepicker({
 		locale: 'de',
 		icons: {
@@ -88,14 +92,17 @@ $(function () {
 		format: "DD.MM.YYYY",
 	});
 
+//EInstellungen des Colorpickers
 	$('#user_backgroundcolor').colorpicker({
 		format: "hex",
 		useAlpha: false,
 	});
 
+//Globale Einstellungen für notify für die Darstellung von Meldungen
 	PNotify.prototype.options.delay = 2000;
 	PNotify.prototype.options.addclass ="stack-bottomright";
 
+//automatische prüfen des Logins
 	login();
 });
 //endregion
@@ -109,6 +116,7 @@ function login() {
 	let username = document.querySelector("#loginUsername").value;
 	let password = document.querySelector("#loginPassword").value;
 
+//Username und password lokal im Browser speichern, für späteren Login sinnvoll
 	if (username !== "") {
 
 		localStorage.setItem("uberListUsername", username);
@@ -147,6 +155,10 @@ function login() {
 	}
 }
 
+/**
+	*	Login submit
+	* Wird ausgelöst wenn Login Button gedrückt wird
+	*/
 $("#form-login").submit(function (e) {
 	e.preventDefault();
 	login();
@@ -164,6 +176,7 @@ $("#form-register").submit(function (e) {
 	let passwordCheck = document.querySelector("#registerPasswordCheck").value;
 	let email = document.querySelector("#registerEmail").value;
 
+//Prüfen ob Passwörter gleich sind
 	if (password !== passwordCheck) {
 		new PNotify({
 			text: 'Die Passwörter stimmen nicht überein.',
@@ -196,6 +209,7 @@ function logout() {
 			document.querySelector("#loginContent").style.display = 'block';
 		});
 
+//Localen Speicher leeren bei Logout
 	localStorage.setItem("uberListUsername", "");
 	localStorage.setItem("uberListPassword", "");
 }
@@ -206,29 +220,34 @@ function logout() {
  * @param element --> DOM Element auf das die Suche angewendet werden soll
  */
 function searchForUserName(search, element) {
-	element.classList.add("loading");
+//erst suchen wenn Begriff > 2 Wörter
+	if(search.length > 2){
+		element.classList.add("loading");
+		myFetch('secure/user/search/'+search, null, "GET")
+			.then(function (data) {
+				//Autocomplete mit Search Daten füllen
+				let source = data.data;
+				$(element).typeahead({source: source});
+			}) // JSON from `response.json()` call
+			.catch(error => console.error(error));
 
-	myFetch('secure/user/search/'+search, null, "GET")
-		.then(function (data) {
-			$(element).typeahead({source: data.data});
-		}) // JSON from `response.json()` call
-		.catch(error => console.error(error));
-
-	element.classList.remove("loading");
+			element.classList.remove("loading");
+	}
 }
+
 //endregion
 
 //region Listen
 /**
  * Bekomme alle Listen
  */
-
 function getLitsts() {
 	myFetch('secure/list', null, "GET")
 		.then(function (data) {
 			let lists = data.lists;
 			let ul = document.querySelector("#listView-list-ul");
 			ul.innerHTML = "";
+			//Aufbauen der Listen
 			for (let i = 0; i < lists.length; i++) {
 				ul.innerHTML += `<li> <a href="javascript:void(0)" onclick="showListDetails('${lists[i]._id}')"><i class="fas fa-pencil-alt"></i></a> <a href="javascript:void(0)" onclick="getElementsFromList('${lists[i]._id}')">${lists[i].title}</a></li>`
 			}
@@ -253,11 +272,14 @@ function showListDetails(id) {
 			loadListMemberShip(list._id);
 		});
 
-
-
+//Details Modal öffnen
 	$('#listDetailsModal').modal("toggle");
 }
 
+/**
+	* Details zu den List-Members wegen geteilter Listen
+	* @param id
+*/
 function loadListMemberShip(id){
 	myFetch('secure/list_membership/'+id, null, "GET")
 		.then(function (data) {
@@ -270,14 +292,23 @@ function loadListMemberShip(id){
 		});
 }
 
+/**
+	* Member zu Liste hinzufügen
+	*	@param username
+*/
 function addMemberToList(username){
 	let id = document.querySelector("#listEditID").value;
-	myFetch('secure/list_membership/'+id, {username: username, listId: id}, "POST")
+	myFetch('secure/list_membership/', {username: username, listId: id}, "POST")
 		.then(function () {
 			loadListMemberShip(id);
 		});
 }
 
+/**
+	* Member von Liste entfernen
+	* @param username
+	* @param id --> ID der Liste von die der User entfernt werden soll
+*/
 //TODO
 function deleteListMemberShip(username,id){
 	myFetch('secure/list_membership/', {username: username,listId: id}, "DELETE")
@@ -422,7 +453,6 @@ function getElementDetails(id) {
 		.then(function (data) {
 			let element = data;
 
-
 			document.querySelector("#elementEditName").value = element.title;
 			document.querySelector("#elementEditOrt").value = element.place;
 			document.querySelector("#elementEditTimePicker").value = moment(element.deadline).format("DD.MM.YYYY");
@@ -430,6 +460,7 @@ function getElementDetails(id) {
 			document.querySelector("#elementEditID").value = element._id;
 			document.querySelector("#elementEditUserSearch").value = element.assignTo;
 
+//Details anzeigen in Modal
 			$('#elementsDetailsModal').modal("toggle");
 		}) // JSON from `response.json()` call
 		.catch(error => console.error(error));
@@ -440,11 +471,20 @@ function getElementDetails(id) {
  * @param id
  */
 function checkElement(id) {
-	document.querySelector("#checkElementSquare_"+id).className = "far fa-check-square";
+	let element = document.querySelector("#checkElementSquare_"+id);
 
-	//TODO check
-	myFetch('secure/list_entry/check/'+id, {isDone: true}, "POST")
-		.catch(error => console.error(error));
+	if(element.classList.contains("fa-check-square")){
+		myFetch('secure/list_entry/check/'+id, {isDone: false}, "POST");
+		element.className = "far fa-square";
+	}
+	else{
+		myFetch('secure/list_entry/check/'+id, {isDone: true}, "POST");
+		element.className = "far fa-check-square";
+	}
+
+//Überlegen ob anders besser?
+//reload, durchstreichen??
+
 }
 
 /**
@@ -463,8 +503,7 @@ function deleteElement() {
 			});
 			$('#elementsDetailsModal').modal("toggle");
 			getElementsFromList(listID);
-		}) // JSON from `response.json()` call
-		.catch(error => console.error(error));
+		});
 }
 
 /**
@@ -491,8 +530,7 @@ function updateElement() {
 			});
 			$('#elementsDetailsModal').modal("toggle");
 			getElementsFromList(listID);
-		}) // JSON from `response.json()` call
-		.catch(error => console.error(error));
+		});
 }
 
 /**
@@ -514,8 +552,7 @@ function addNewElement(element) {
 			.then(function (data) {
 				getElementsFromList(listID);
 				element.value = "";
-			}) // JSON from `response.json()` call
-			.catch(error => console.error(error));
+			});
 	}
 }
 //endregion
@@ -536,15 +573,7 @@ function getUserData() {
 			document.querySelector("#user_email").value = user.email;
 			document.querySelector("#user_backgroundcolor").value = user.color;
 			document.querySelector("#user_geburtsdatum").value = moment(user.gebDate).format("DD.MM.YYYY");
-
-
-			document.querySelector("body").style.background = user.color;
-
-			//console.log(document.querySelector("body").style.background);
-
-
-		}) // JSON from `response.json()` call
-		.catch(error => console.error(error));
+		});
 }
 
 $("#form-user-data").submit(function(e){
@@ -576,8 +605,8 @@ $("#form-user-data").submit(function(e){
 					type: 'success'
 				});
 				getUserData();
-			})
-			.catch(error => console.error(error));
+				setBackgroundColor(data.color);
+			});
 	}
 	else{
 		new PNotify({
